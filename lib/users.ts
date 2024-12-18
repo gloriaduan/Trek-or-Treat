c781845243bb177c33f62e8c79afde31f150110f
Gloria <gloriaduan29@gmail.com>
2024-12-18 17:26:14 -0500

    added webhooks to sync clerk user with db

---
+++ lib/users.ts
@@ -0,0 +1,42 @@
+import prisma from '@/lib/db';
+import {User} from '@prisma/client';
+
+export async function createUser(data: User) {
+    try {
+        const user = await prisma.user.create({data});
+        return {user};
+    }catch (e) {
+        console.log(`error occurred: ${e}`);
+        return {error: e};
+    }
+}
+
+export async function getUserByClerkId(data: string) {
+    try{
+        const user = await prisma.user.findUnique({where: {clerkUserId: data}});
+        return {user};
+    }catch (e) {
+        console.log(`error occurred: ${e}`);
+        return {error: e}
+    }
+}
+
+export async function updateUser(id: string, data: Partial<User>) {
+    try {
+        const user = await prisma.user.update({
+            where: {
+                clerkUserId: id
+            },
+            data
+        })
+
+        return {user};
+    }catch(e) {
+        console.log(`error occurred: ${e}`);
+        return {error: e};
+    }
+}
+
+export async function deleteUser(id: string) {
+
+}
