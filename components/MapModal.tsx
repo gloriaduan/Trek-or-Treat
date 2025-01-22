02a7dee5d124173685c8e2f81883df128080f150
Gloria Duan <gloriaduan29@gmail.com>
2025-01-21 21:27:42 -0500

    added slider. image needs to be fixed

--- components/MapModal.tsx
+++ components/MapModal.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Dialog,
   DialogContent,
@@ -7,6 +9,10 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { useLocationStore, useStartStore } from "@/store/app-store";
+
+import useEmblaCarousel from "embla-carousel-react";
+import Image from "next/image";
+import { useCallback } from "react";
 
 interface CustomLocation {
   id: string;
@@ -28,6 +34,15 @@ function MapModal({ isOpen, setIsOpen, currLocation }: MapModalProps) {
   const start = useStartStore((state) => state.start);
   const addStart = useStartStore((state) => state.addStart);
   const addDestination = useLocationStore((state) => state.addDestination);
+  const [emblaRef, emblaApi] = useEmblaCarousel();
+
+  const scrollPrev = useCallback(() => {
+    if (emblaApi) emblaApi.scrollPrev();
+  }, [emblaApi]);
+
+  const scrollNext = useCallback(() => {
+    if (emblaApi) emblaApi.scrollNext();
+  }, [emblaApi]);
 
   const handleAddDestination = () => {
     const destination = {
@@ -55,6 +70,28 @@ function MapModal({ isOpen, setIsOpen, currLocation }: MapModalProps) {
         <DialogHeader>
           <DialogTitle>{currLocation.address}</DialogTitle>
           <DialogDescription>{currLocation.description}</DialogDescription>
+          <div className="embla p-5" ref={emblaRef}>
+            <div className="embla__container">
+              {isOpen &&
+                currLocation.images.map((image, index) => (
+                  <div key={index} className="embla__slide">
+                    <Image
+                      alt="image"
+                      width={"auto"}
+                      height={"auto"}
+                      sizes="100vw"
+                      src={`${image}`}
+                    />
+                  </div>
+                ))}
+            </div>
+            <button className="embla__prev" onClick={scrollPrev}>
+              Prev
+            </button>
+            <button className="embla__next" onClick={scrollNext}>
+              Next
+            </button>
+          </div>
           <button className="btn-primary" onClick={handleAddDestination}>
             Add Destination
           </button>
